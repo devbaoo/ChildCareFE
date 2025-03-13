ddc6ffceb48ec9421b2ea0cbd92895396d5d22fc
hungdz369@gmail.com <hungdz369@gmail.com>
2025-03-13 20:13:56 +0700

    first commit

---
+++ src/routers/index.tsx
@@ -0,0 +1,14 @@
+
+import Home from "@/page/Home";
+import { Route, Routes } from "react-router-dom";
+
+
+const AppRouter = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
+    );
+}
+
+export default AppRouter;
